997d60062887df75e3a70c4addcb17a54c4fad9e
wen yo <wenda897436@gmail.com>
2021-11-16 23:43:45 +0800

    add sass & google api

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
 <template>
-  <router-view />
+  <div id="map">
+    <GMapMap :center="center" :zoom="7" map-type-id="terrain">
+      <GMapCluster>
+        <GMapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center = m.position"
+        />
+      </GMapCluster>
+    </GMapMap>
+  </div>
 </template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      center: { lat: 51.093048, lng: 6.84212 },
+      markers: [
+        {
+          position: {
+            lat: 51.093048,
+            lng: 6.84212,
+          },
+        },
+        {
+          position: {
+            lat: 52.093048,
+            lng: 6.84212,
+          },
+        }, // Along list of clusters
+      ],
+    };
+  },
+};
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+.vue-map-container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import VueGoogleMaps from "@fawmi/vue-google-maps";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(VueGoogleMaps, {
+    load: {
+      key: "AIzaSyBD5zZfLsBKn7bZThJUoP8wkJn6shPhXkM",
+    },
+  })
+  .mount("#app");
