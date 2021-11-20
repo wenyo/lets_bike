c9561b896ee5bccf521e55730a7fc7f0cf03bf18
wen.you <wen.you@nextfortune.tw>
2021-11-21 00:46:20 +0800

    add button

---
+++ src/component/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="clickFun">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "Button",
+    },
+    clickFun: {
+      type: Function,
+      default: () => {},
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+button {
+  cursor: pointer;
+  border: unset;
+  color: $basic-color-hover;
+  background: $primary-color-second;
+  font-size: 14px;
+  padding: 10px 100px;
+  border-radius: 8px;
+  transition: .3s;
+
+  &:hover {
+    background: $gradient-color-primary;
+  }
+}
+</style>
