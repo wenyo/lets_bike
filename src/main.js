e0e472575dd3d90d1d71fae08d20e6b9a87a2019
wen.you <wen.you@nextfortune.tw>
2021-11-20 20:29:43 +0800

    add color style

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import VueGoogleMaps from "@fawmi/vue-google-maps";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/scss/reset.css"
+import "./assets/scss/basic.scss"
+import "./assets/fonts/font.css"
 import "./assets/icon/style.css"
 
 createApp(App)
