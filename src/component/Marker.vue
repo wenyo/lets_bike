ea00f70fe9b4c990988dac647978cda1412ea46a
wen.you <wen.you@nextfortune.tw>
2021-11-20 17:24:32 +0800

    marker style

--- src/component/Marker.vue
+++ src/component/Marker.vue
@@ -1,33 +1,44 @@
 <template>
   <div class="marker">
-    <Icon :icon="`marker_${shape}`" color="yellow-300" />
-    <Icon icon="attraction" color="dark" v-if="content !== content_mode[2]" />
-    <slot v-else />
+    <img v-if="type === type_mode[0]" src="@/assets/img/station.svg" alt="" />
+
+    <template v-else>
+      <Icon :icon="`marker_${type}`" :color="'yellow-900'" />
+      <slot v-if="content === content_mode[2]" />
+      <Icon v-else :icon="content" :color="contentColor" />
+    </template>
   </div>
 </template>
 
 <script>
 import Icon from "./Icon.vue";
 
-const shape_mode = ["circle", "bubble"];
+const type_mode = ["red", "circle", "bubble", ];
 const content_mode = ["attraction", "station", "text"];
 
 export default {
   props: {
-    shape: {
+    type: {
       type: String,
-      default: shape_mode[1],
-      validator: (val) => shape_mode.includes(val),
+      default: type_mode[0],
+      validator: (val) => type_mode.includes(val),
     },
     content: {
       type: String,
       default: content_mode[0],
       validator: (val) => content_mode.includes(val),
     },
+    color: {
+      type: String,
+    },
+    contentColor: {
+      type: String,
+    },
   },
   data() {
     return {
       content_mode,
+      type_mode,
     };
   },
   components: {
@@ -41,11 +52,15 @@ export default {
   width: fit-content;
   position: relative;
 
-  & :first-child {
+  img {
+    width: 35px;
+  }
+  
+  & i:first-child {
     font-size: 40px;
   }
 
-  & :last-child {
+  & i:last-child {
     position: absolute;
     top: 50%;
     left: 50%;
