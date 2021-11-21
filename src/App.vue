6e3164b7b22c1d931f59f76366914495331fbddc
wen.you <wen.you@nextfortune.tw>
2021-11-21 13:44:59 +0800

    get position

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <Nav />
   <router-view></router-view>
-
 </template>
 <script>
 import Nav from "./component/Nav.vue";
 export default {
   components: {
     Nav,
+  },
+  data() {
+    return {
+      pos: {
+        lat: 0,
+        lng: 0,
+      },
+    };
+  },
+  mounted() {
+    this.findMe();
+  },
+  methods: {
+    findMe() {
+      navigator.geolocation.getCurrentPosition(this.posGetter, (error) => {
+        console.error(error);
+      });
+    },
+    posGetter(pos) {
+      const crd = pos.coords;
+      console.log("crd");
+      console.log(crd);
+      this.pos = {
+        lat: crd.latitude,
+        lng: crd.longitude,
+      };
+    },
   },
 };
 </script>
